2a81dea3a071a1739e0bbb7aa35afd61ee97890a
tomi lotila <lotilatomi@gmail.com>
2023-02-24 22:13:56 +0200

    added magnet-link support

--- auto_downloader.sh
+++ auto_downloader.sh
@@ -1,8 +1,16 @@
 
 
 while true; do 
+	files=*.magnet; 
+	aria2c --bt-metadata-only=true --bt-save-metadata=true <$files; 
+	rm $files; 
 	files=*.torrent; 
 	aria2c --seed-time $((60*24)) --max-concurrent-downloads=100 $files; 
 	rm $files; 
 	sleep 60m; 
 done;
+
+
+
+
+
