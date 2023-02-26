09fe0a7d55eaed451a4f4fdac7191a739614a028
tomi lotila <lotilatomi@gmail.com>
2023-02-26 16:39:10 +0200

    fixed magnet links

--- auto_downloader.sh
+++ auto_downloader.sh
@@ -1,16 +1,14 @@
 
-
 while true; do 
-	files=*.magnet; 
-	aria2c --bt-metadata-only=true --bt-save-metadata=true <$files; 
-	rm $files; 
-	files=*.torrent; 
-	aria2c --seed-time $((60*24)) --max-concurrent-downloads=100 $files; 
-	rm $files; 
-	sleep 60m; 
+
+aria2c --seed-time $((60*24)) --max-concurrent-downloads=100 $(<*.magnet); 
+
+aria2c --seed-time $((60*24)) --max-concurrent-downloads=100 *.torrent; 
+
+rm *.torrent; 
+
+rm *.magnet; 
+
+sleep 60m; 
+
 done;
-
-
-
-
-
